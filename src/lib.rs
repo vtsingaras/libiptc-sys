18774395bfb682f92d028adbb68b12af604c0839
Vyronas Tsingaras <vyronas@vtsingaras.me>
2016-09-21 19:51:24 +0300

    Ran rustfmt.

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,7 @@ const RS_CHAINLABEL_MAX_SIZE: usize = 32;
 pub type in_addr_t = u32;
 
 #[repr(C)]
-pub struct in_addr
-{
+pub struct in_addr {
     s_addr: in_addr_t,
 }
 
@@ -63,30 +62,77 @@ extern "C" {
     pub fn iptc_first_chain(handle: *mut xtc_handle) -> *const c_char;
     pub fn iptc_next_chain(handle: *mut xtc_handle) -> *const c_char;
     pub fn iptc_first_rule(chain: *const c_char, handle: *mut xtc_handle) -> *const ipt_entry;
-    pub fn iptc_next_rule(prev: *const ipt_entry, handle: *mut xtc_handle ) -> *const ipt_entry;
+    pub fn iptc_next_rule(prev: *const ipt_entry, handle: *mut xtc_handle) -> *const ipt_entry;
     pub fn iptc_get_target(e: *const ipt_entry, handle: *mut xtc_handle) -> *const c_char;
     pub fn iptc_builtin(chain: *const c_char, handle: *const xtc_handle) -> c_int;
-    pub fn iptc_get_policy(chain: *const c_char, counter: *mut xt_counters, handle: *mut xtc_handle) -> *const c_char;
-    pub fn iptc_insert_entry(chain: xt_chainlabel, e: *const ipt_entry, rulenum: c_uint, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_replace_entry(chain: xt_chainlabel, e: *const ipt_entry, rulenum: c_uint, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_append_entry(chain: xt_chainlabel, e: *const ipt_entry, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_check_entry(chain: xt_chainlabel, origfw: *const ipt_entry, matchmask: *mut u8, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_delete_entry(chain: xt_chainlabel, origfw: *const ipt_entry, matchmask: *mut u8, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_delete_num_entry(chain: xt_chainlabel, rulenum: c_uint, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_check_packet(chain: xt_chainlabel, entry: *mut ipt_entry, handle: *mut xtc_handle) -> *const c_char;
+    pub fn iptc_get_policy(chain: *const c_char,
+                           counter: *mut xt_counters,
+                           handle: *mut xtc_handle)
+                           -> *const c_char;
+    pub fn iptc_insert_entry(chain: xt_chainlabel,
+                             e: *const ipt_entry,
+                             rulenum: c_uint,
+                             handle: *mut xtc_handle)
+                             -> c_int;
+    pub fn iptc_replace_entry(chain: xt_chainlabel,
+                              e: *const ipt_entry,
+                              rulenum: c_uint,
+                              handle: *mut xtc_handle)
+                              -> c_int;
+    pub fn iptc_append_entry(chain: xt_chainlabel,
+                             e: *const ipt_entry,
+                             handle: *mut xtc_handle)
+                             -> c_int;
+    pub fn iptc_check_entry(chain: xt_chainlabel,
+                            origfw: *const ipt_entry,
+                            matchmask: *mut u8,
+                            handle: *mut xtc_handle)
+                            -> c_int;
+    pub fn iptc_delete_entry(chain: xt_chainlabel,
+                             origfw: *const ipt_entry,
+                             matchmask: *mut u8,
+                             handle: *mut xtc_handle)
+                             -> c_int;
+    pub fn iptc_delete_num_entry(chain: xt_chainlabel,
+                                 rulenum: c_uint,
+                                 handle: *mut xtc_handle)
+                                 -> c_int;
+    pub fn iptc_check_packet(chain: xt_chainlabel,
+                             entry: *mut ipt_entry,
+                             handle: *mut xtc_handle)
+                             -> *const c_char;
     pub fn iptc_flush_entries(chain: xt_chainlabel, handle: *mut xtc_handle) -> c_int;
     pub fn iptc_zero_entries(chain: xt_chainlabel, handle: *mut xtc_handle) -> c_int;
     pub fn iptc_create_chain(chain: xt_chainlabel, handle: *mut xtc_handle) -> c_int;
     pub fn iptc_delete_chain(chain: xt_chainlabel, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_rename_chain(oldname: xt_chainlabel, newname: xt_chainlabel, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_set_policy(chain: xt_chainlabel, policy: xt_chainlabel, counters: *mut xt_counters, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_get_references(references: *mut c_uint, chain: xt_chainlabel, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_read_counter(chain: xt_chainlabel, rulenum: c_uint, handle: *mut xtc_handle) -> *mut xt_counters;
-    pub fn iptc_zero_counter(chain: xt_chainlabel, rulenum: c_uint, handle: *mut xtc_handle) -> c_int;
-    pub fn iptc_set_counter(chain: xt_chainlabel, rulenum: c_uint, counters: *mut xt_counters, handle: *mut xtc_handle) -> c_int;
+    pub fn iptc_rename_chain(oldname: xt_chainlabel,
+                             newname: xt_chainlabel,
+                             handle: *mut xtc_handle)
+                             -> c_int;
+    pub fn iptc_set_policy(chain: xt_chainlabel,
+                           policy: xt_chainlabel,
+                           counters: *mut xt_counters,
+                           handle: *mut xtc_handle)
+                           -> c_int;
+    pub fn iptc_get_references(references: *mut c_uint,
+                               chain: xt_chainlabel,
+                               handle: *mut xtc_handle)
+                               -> c_int;
+    pub fn iptc_read_counter(chain: xt_chainlabel,
+                             rulenum: c_uint,
+                             handle: *mut xtc_handle)
+                             -> *mut xt_counters;
+    pub fn iptc_zero_counter(chain: xt_chainlabel,
+                             rulenum: c_uint,
+                             handle: *mut xtc_handle)
+                             -> c_int;
+    pub fn iptc_set_counter(chain: xt_chainlabel,
+                            rulenum: c_uint,
+                            counters: *mut xt_counters,
+                            handle: *mut xtc_handle)
+                            -> c_int;
     pub fn iptc_commit(handle: *mut xtc_handle) -> c_int;
     pub fn iptc_get_raw_socket() -> c_int;
     pub fn iptc_strerror(err: c_int) -> *const c_char;
     pub fn dump_entries(handle: *mut xtc_handle);
 }
-
